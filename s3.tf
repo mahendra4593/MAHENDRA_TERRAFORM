5a156262da908cefe8dc12b6ef2803ecc153806b
mahendra4593 <madan452218@gmail.com>
2025-03-15 17:15:35 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,11 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "s3bucketcreation" {
   bucket = "swiggy-bucket123-bymahendra"
+}
 
-  # Enable versioning
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.s3bucketcreation.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
